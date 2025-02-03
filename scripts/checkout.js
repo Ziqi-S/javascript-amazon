8a25bd8fc939c2044b00a85d56eae7f358245236
Ziqi-S <louise_shui@outlook.com>
2025-02-03 17:04:20 +0100

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,7 +5,20 @@ import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js'
 
-loadProducts(() => {
+/*
+- better way to handle asynchronous code
+- let us wait for some code to finish before going to the next step
+- runs the inner function immediatly
+- resolve is a function
+    - let us control when to go the the next step
+*/
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('resolve can pass values to the next step');
+    });
+}).then((value) => {//next step after loadProducts()
+    console.log(value)
+    
     renderOrderSummary();
     renderPaymentSummary();
-});
+})
